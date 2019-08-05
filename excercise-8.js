7b9d949a6e33e1e7e0e7f5e3fefad4961fe79d93
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-05 23:52:50 +0700

    add excercise 8

---
+++ excercise-8.js
@@ -0,0 +1,20 @@
+function pasanganTerbesar(num) {
+    var str = String(num);
+    var angka;
+    var pembanding = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        angka = Number(str[i] + str[i + 1]);
+        if (angka > pembanding) {
+            pembanding = angka;
+        }
+    }
+    return pembanding;
+}
+  
+  // TEST CASES
+  console.log(pasanganTerbesar(641573)); // 73
+  console.log(pasanganTerbesar(12783456)); // 83
+  console.log(pasanganTerbesar(910233)); // 91
+  console.log(pasanganTerbesar(71856421)); // 85
+  console.log(pasanganTerbesar(79918293)); // 99
