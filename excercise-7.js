817c5cffc232a797436cef42ccfc9697159e05f4
Arnold Therigan <53247820+arnoldtherigan15@users.noreply.github.com>
2019-08-22 20:44:26 +0700

    Update excercise-7.js

--- excercise-7.js
+++ excercise-7.js
@@ -1,18 +1,33 @@
-function hitungJumlahKata(kalimat) {
-    var jumlahKata = 0;
-    var spasi = 0;
-    for (var i = 0; i < kalimat.length; i++) {
-        if (kalimat[i] === ' ') {
-            spasi += 1;
+function hitungJumlahKata(str) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        var isFind = false;
+        if (str[i] === ' ') {
+            isFind = true;
+            if (i === 0 || str[i + 1] === ' ') {
+                isFind = false;
+            }
+
         }
+        if (i === str.length - 1) {
+            if (str[i] === ' ') {
+                isFind = false;
+            }
+            count++;
+        }
+        if (isFind === true) {
+            count++;
+
+        }
+
     }
-    jumlahKata = spasi + 1;
-    return jumlahKata;
-  }
+    return count;
+}
   
-  // TEST CASES
-  console.log(hitungJumlahKata('I have a dream')); // 4
-  console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
-  console.log(hitungJumlahKata('A song to sing')); // 4
-  console.log(hitungJumlahKata('I')); // 1
-  console.log(hitungJumlahKata('I believe I can code')); // 5
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
+console.log(hitungJumlahKata(' I have a dream a   song to    sing     ')); // 8
